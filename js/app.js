a8c7dcaf2cc543a5b3a7b3271d859d953f3d135d
unknown <rbroch@ius.edu>
2017-10-31 19:42:47 -0400

    added more stuff

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
-    $("#trigger").click(function() {
-        $("#demo").html("Hello, World!");
-    });
-});
+/********************************
+JavaScript Function
+********************************/
+function submitFunction(event) {
+
+var x= document.forms["myForm"]["Name"].value;
+var y= document.forms["myForm"]["email"].value;    
+
+if(x === null ||x === ""||y === null||y === "")
+{
+alert("One or more fields is empty!");
+
+    event.preventDefault();
+
+  }
+ else {
+ alert ("Successfully Submitted!");
+}
+}
+
+
+
+
+
+
